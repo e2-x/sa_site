adbbc2176d5788a2f639ff5d43205c72e81dabb7
e2 <e2.tejeda@gmail.com>
2020-02-12 15:31:39 -0500

    More server stuff.
    Learning how to correctly deal with API calls and consuming them

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo_v2 from '../img/logo_v2.png';
 import { Route, NavLink, HashRouter } from 'react-router-dom';
 
 import '../style/App.css';
-
 import Nav from './nav'
 import About from './about'
 import Work from './work'
 
+// class App extends Component {
+
+//   state = {
+//     response: '',
+//     post: '',
+//     responseToPost: '',
+//   };
+
+//   componentDidMount(){
+//     this.callApi()
+//       .then(res => this.setState({ response: res.express }))
+//       .catch(err => console.log(err));
+//   }
+
+//   callApi = async () => {
+//     const response = await fetch('/api/hello');
+//     const body = await response.json();
+//     if(response.status !== 200) {
+//       process.on('uncaughtException');
+//       throw Error(body.message);
+//     }
+
+//     return body;
+//   };
+
+//   handleSubmit = async e => {
+//     e.preventDefault();
+//     const response = await fetch('api/world', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ post: this.state.post }),
+//     });
+//     const body = await response.text();
+//     this.setState({ responseToPost: body});
+//   };
+
+// render() {
+//     return (
+//       <div className="App">
+//         <p>{this.state.response}</p>
+//           <form onSubmit={this.handleSubmit}>
+//             <p>
+//               <strong>Post to Server:</strong>
+//             </p>
+//             <input
+//               type="text"
+//               value={this.state.post}
+//               onChange={e => this.setState({ post: e.target.value })}
+//             />
+//             <button type="submit">Submit</button>
+//           </form>
+//         <p>{this.state.responseToPost}</p>
+
+//         <header className="App-header">
+//           <img src={logo_v2} className="App-logo" alt="logo" />
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// } 
 function App() {
   return (
     <HashRouter>
--- src/components/work.js
+++ src/components/work.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Gallery from 'react-grid-gallery'
 
-function work(){
-    return (
-        <div>
-            <h3>Work</h3>
-        </div>
-    );
+import '../style/App.css';
+
+const IMAGES = 
+        [{
+            src: 'http://i.imgur.com/5bMgMvP.png',
+            thumbnail: 'http://i.imgur.com/5bMgMvP.png',
+            thumbnailWidth: 320,
+            thumbnailHeight: 174
+        }]
+
+class work extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:3000/api/hello")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+
+    componentWillMount() {
+        this.callAPI();
+    }
+    
+    render(){
+        return (
+            <div className="gallery_div">
+                <h3>Work</h3>
+                <Gallery images={IMAGES}/>
+            </div>
+        );
+    }
 }
 
 export default work;
